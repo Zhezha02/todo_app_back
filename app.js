56b203464d8f546befa3ef8b0b0a873ed6def936
Evgeniya <evgeniyakirtoka@gmailcom>
2021-04-15 09:12:16 +0300

    Add error handler mw

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = require('./routers');
+const errorHandler = require('./middlewares/error.handlers.mw');
 
 const app = express();
 
 app.use(express.json());
 app.use('api', router);
+
+app.use(errorHandler);
 
 module.exports = app;
---
+++ middlewares/error.handlers.mw.js
@@ -0,0 +1,6 @@
+module.exports = async (err, req, res, next) => {
+  const status = err.status || 500;
+  res.send(status).send({
+    errors: [{ message: err.message || 'Server error' }],
+  });
+};
